773b5691614cdae850f062290ef35eef8e9a36f4
Jiri Knesl <jiri@sprintmasters.cz>
2013-02-05 14:14:16 +0100

    test for "can filter users by timestamp"

--- bin/cogniveo/analytics_test.clj
+++ bin/cogniveo/analytics_test.clj
@@ -4,9 +4,9 @@
 
 (defn state [] [
 		{:user {:id "tillda"} :experiment {:name "Glog" :params {}}}		
-		{:user {:id "jirka"} :experiment {:name "Abc" :params {}}}
-		{:user {:id "jirka"} :experiment {:name "Def" :params {:foo "bar"}}}
-		{:user {:id "jirka"} :experiment {:name "Def" :params {}}}
+		{:user {:id "jirka"} :experiment {:name "Abc" :params {}} :timestamp 90}
+		{:user {:id "jirka"} :experiment {:name "Def" :params {:foo "bar"}} :timestamp 110}
+		{:user {:id "jirka"} :experiment {:name "Def" :params {}} :timestamp 120}
 	])
 
 (fact
@@ -19,3 +19,7 @@
 	"can parse dot-notation params"
 	(get-params {"param.foo" "foovalue" "param.bar" "barvalue" "a" 1 "b" 2}) => {:foo "foovalue" :bar "barvalue"}
 )
+
+(fact
+  "can filter users by timestamp"
+  (count (get-experiments (state) "jirka" "" {} {:from 100 :to 120})) => 2)
