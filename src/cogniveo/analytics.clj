1fc61e901d114ae6ab7fc527bf3267023d09c6eb
Michal Till <michal.till@gmail.com>
2013-02-05 13:44:10 +0100

    Can query by user, name and params

--- src/cogniveo/analytics.clj
+++ src/cogniveo/analytics.clj
@@ -1,4 +1,15 @@
 (ns cogniveo.analytics)
+(use 'clojure.data)
 
-(defn get-experiments [state user-id]
-  (filter (fn [row] (= user-id ((row :user) :id))) state))
+(defn get-experiments 
+	([state user-id] (get-experiments state user-id "" {}))
+	([state user-id experiment-name] (get-experiments state user-id experiment-name {}))
+	([state user-id experiment-name params] (filter (fn [row] 
+		(and
+			(= user-id ((row :user) :id))
+			(or (= experiment-name "") (= experiment-name ((row :experiment) :name)))
+			(= (nth (diff ((row :experiment) :params) params) 1) nil)
+			 
+	)) state)
+	)
+)
--- test/cogniveo/analytics_test.clj
+++ test/cogniveo/analytics_test.clj
@@ -3,14 +3,14 @@
         cogniveo.analytics))
 
 (defn state [] [
-			{:user {:id "tillda"} :experiment {:name "Glog"}}
-			{:user {:id "jirka"} :experiment {:name "Abc"}}
-      {:user {:id "jirka"} :experiment {:name "Def"}}
-      {:user {:id "jirka"} :experiment {:name "Def"}}
-		])
+		{:user {:id "tillda"} :experiment {:name "Glog" :params {}}}		
+		{:user {:id "jirka"} :experiment {:name "Abc" :params {}}}
+		{:user {:id "jirka"} :experiment {:name "Def" :params {:foo "bar"}}}
+		{:user {:id "jirka"} :experiment {:name "Def" :params {}}}
+	])
 
 (fact
 	"we can list users' experiments"
-	(count (get-experiments (state) {:user "tillda"})) => 1
-  (count (get-experiments (state) {:user "jirka" :name "Def"})) => 2)
-
+	(count (get-experiments (state) "jirka")) => 3
+	(count (get-experiments (state) "jirka" "Def")) => 2
+	(count (get-experiments (state) "jirka" "" {:foo "bar"})) => 1)
