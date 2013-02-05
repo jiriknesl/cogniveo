ba72639b4ef83a37ac21ce942626ca745e9e1ef1
Michal Till <michal.till@gmail.com>
2013-02-05 13:49:50 +0100

    get-params test

--- test/cogniveo/analytics_test.clj
+++ test/cogniveo/analytics_test.clj
@@ -14,3 +14,8 @@
 	(count (get-experiments (state) "jirka")) => 3
 	(count (get-experiments (state) "jirka" "Def")) => 2
 	(count (get-experiments (state) "jirka" "" {:foo "bar"})) => 1)
+
+(fact
+	"can parse dot-notation params"
+	(get-params {:param.foo "foovalue" :param.bar "barvalue" :a 1 :b 2}) => {:foo "foovalue" :bar "barvalue"}
+)
