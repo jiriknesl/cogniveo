956e7bb6935f37dc8c1e5e346a972c3665e1fc15
Jiri Knesl <jiri@sprintmasters.cz>
2013-02-05 10:55:59 +0100

    it should filter by user name and project name

--- test/cogniveo/analytics_test.clj
+++ test/cogniveo/analytics_test.clj
@@ -3,11 +3,14 @@
         cogniveo.analytics))
 
 (defn state [] [
-			{:user {:id "tillda"}},
-			{:user {:id "jirka"}},
+			{:user {:id "tillda"} :experiment {:name "Glog"}}
+			{:user {:id "jirka"} :experiment {:name "Abc"}}
+      {:user {:id "jirka"} :experiment {:name "Def"}}
+      {:user {:id "jirka"} :experiment {:name "Def"}}
 		])
 
 (fact
 	"we can list users' experiments"
-	(count (get-experiments (state) "tillda")) => 1)
+	(count (get-experiments (state) {:user "tillda"})) => 1
+  (count (get-experiments (state) {:user "jirka" :name "Def"})) => 2)
 
